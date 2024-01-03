2ce04b6be79c07e581f82c85a121304db0895136
EmilyMontalvo25 <emily.montalvo@epn.edu.ec>
2024-01-03 03:02:32 -0500

    Add Intents

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.b2023gr2sw
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    val callbackContenidoIntenExplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            if(result.data != null){
+                //logica de negocio
+                val data =  result.data
+                "${data?.getStringExtra("nombreModificado")}"
+            }
+        }
+    }
+
+    val callbackIntentPickUri =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if (result.resultCode == RESULT_OK){
+                if(result.data != null){
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri, null, null, null, null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        mostrarSnackbar("Telefono ${telefono}")
+
+                    }
+
+                    val data =  result.data
+                    mostrarSnackbar("${data?.getStringExtra("nombreModificado")}")
+
+                }
+            }
+
+
+        }
+
+    fun mostrarSnackbar(texto:String){
+        val snack = Snackbar.make(findViewById(R.id.id_layout_main), texto, Snackbar.LENGTH_LONG)
+        snack.show()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,7 +74,31 @@ class MainActivity : AppCompatActivity() {
         botonListView.setOnClickListener{
             irActividad(BListView::class.java)
         }
+
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito.setOnClickListener {
+            val intentConRespuesta = Intent(
+                Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            callbackIntentPickUri.launch(intentConRespuesta)
+        }
+
+        val botonIntentExplicito = findViewById<Button>(R.id.btn_ir_intent_explicito)
+        botonIntentExplicito.setOnClickListener {
+            abrirActividadConParametros(CIntentExplicitoParametros::class.java)
+        }
     }
+
+    fun abrirActividadConParametros(clase: Class<*>) {
+        val intentExplicito = Intent(this, clase)
+        //Enviar parametros(solamente variables primitivas)
+        intentExplicito.putExtra("nombre", "Adrian")
+        intentExplicito.putExtra("apellido", "Eguez")
+        intentExplicito.putExtra("edad", 34)
+        callbackContenidoIntenExplicito.launch(intentExplicito)
+
+    }
+
     fun irActividad(
         clase:Class<*>
     ){
