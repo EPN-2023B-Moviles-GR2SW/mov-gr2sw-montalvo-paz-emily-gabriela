b6bc5c39d5ce9a5b45bf1b6fc6bdc4a2fdfa6558
EmilyMontalvo25 <emily.montalvo@epn.edu.ec>
2024-01-15 01:16:23 -0500

    Add layouts

---
+++ 04-ExamenBI/app/src/main/java/com/example/a04_examenbi/EmpleadoHome.kt
@@ -0,0 +1,11 @@
+package com.example.a04_examenbi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmpleadoHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_empleado_home)
+    }
+}
